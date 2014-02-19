8e806cb269d3c2e3953c2bc94c89ecd07c7b09f1
Javi Lagunas <jalp@tid.es>
2014-02-19 15:54:56 +0100

    FIX settings data

--- settings.py
+++ settings.py
@@ -6,5 +6,7 @@ import multiprocessing
 bind = '0.0.0.0:8000'
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = 'gevent'
-debug = True
+loglevel = 'debug'
 backlog = 2048
+errorlog = '/opt/bvp/log/gunicorn-error.log'
+accesslog = '/opt/bvp/log/gunicorn-access.log'
